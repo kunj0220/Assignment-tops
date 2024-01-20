89c588afea5cc3638e5022021a801b3e335d3cda
kunj0220 <156287019+kunj0220@users.noreply.github.com>
2024-01-20 23:23:23 +0530

    Add files via upload

---
+++ module 1/MODULE1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("NAME:- PARMAR KUNJ J ");
+	printf("\nBIRTHDAY:- 22/12/1999");
+	printf("\nAGE:- 24");
+	printf("\nADDRESS:- 29,SOMNATH SOC \n \t NR.UJALA CIRCLE \n \t SARKHEJ AHMEDABAD ");
+	return 0;
+	
+}
